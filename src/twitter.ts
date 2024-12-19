10f56c00ba47b34f68416f8a1bb361a3828ffaf8
femshima <49227365+femshima@users.noreply.github.com>
2024-12-20 08:03:39 +0900

    Improve RSS diff generator

--- src/twitter.ts
+++ src/twitter.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { setTimeout } from "timers/promises";
-import Parser from "rss-parser";
+import Parser, { type Item } from "rss-parser";
 import { TwitterApi, type TwitterApiTokens } from "twitter-api-v2";
 import * as core from "@actions/core";
 
@@ -15,10 +15,25 @@ export async function tweetRssDiff(
   const oldRss = await parseRss(parser, rssPaths.oldRssPath);
   const newRss = await parseRss(parser, rssPaths.newRssPath);
 
-  const oldEntries = new Set(oldRss.items.map((item) => item.link));
+  const oldIdent = oldRss.items.map(generateIdents).reduce(
+    (acc, idents) => {
+      Object.entries(idents).forEach(
+        ([key, value]) => value && acc[key as keyof Identifier].add(value),
+      );
+      return acc;
+    },
+    {
+      dateLinkIdents: new Set(),
+      dateTitleIdents: new Set(),
+    },
+  );
 
   const posts = newRss.items
-    .filter((item) => !oldEntries.has(item.link))
+    .filter((item) => {
+      return Object.entries(generateIdents(item)).every(
+        ([key, value]) => !oldIdent[key as keyof Identifier].has(value),
+      );
+    })
     .map((entry) => `${entry.title} ${entry.link}`);
   if (posts.length === 0) {
     core.info("No new entry found.");
@@ -49,4 +64,30 @@ type RssData = Parser.Output<{}>;
 async function parseRss(parser: Parser, filePath: string): Promise<RssData> {
   const data = fs.readFileSync(filePath, "utf8");
   return await parser.parseString(data);
+}
+
+
+interface Identifier {
+  dateLinkIdents?: string;
+  dateTitleIdents?: string;
+}
+
+/**
+ * Generates an identifier object for a given RSS feed item.
+ *
+ * @param item - The RSS feed item for which to generate identifiers.
+ * @returns An object containing identifiers based on the item's publication date, link, and title.
+ */
+function generateIdents(item: Item): Identifier {
+  const date = item.pubDate && new Date(item.pubDate);
+  const dateString = date ? date.toDateString() : "";
+
+  const result: Identifier = {};
+
+  if (typeof item.link === "string") {
+    result.dateLinkIdents = `${dateString};;;${item.link}`;
+  }
+  result.dateTitleIdents = `${dateString};;;${item.title}`;
+
+  return result;
 }
